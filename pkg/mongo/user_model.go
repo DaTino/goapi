62f16c8ddee62d7d6f84c7c0a93506432aeb1d73
DaTino <36283321+DaTino@users.noreply.github.com>
2020-07-27 15:50:31 -0500

    completed user_model

---
+++ pkg/mongo/user_model.go
@@ -0,0 +1,36 @@
+import (
+  "go_rest_api/pkg"
+  "gopkg.in/mgo.v2/bson"
+  "gopjg.in/mgo.v2"
+)
+
+type userModel struct {
+  Id        bson.ObjectId `bson:"_id,omitempty"`
+  Username  string
+  Password  string
+}
+
+func userModelIndex() mgo.Idex {
+  return mgo.Index{
+    Key:        []string{"username"},
+    Unique:     true,
+    DropDups:   true,
+    Background: true,
+    Sparse:     true,
+  }
+}
+
+func newUserModel(u *root.User) *userModel {
+  return &userModel{
+    Username: u.Username,
+    Password: u.Password
+  }
+}
+
+func(u *userModel) toRootUser() *root.User {
+  return &root.User{
+    Id: u.Id.Hex(),
+    Username: u.Username,
+    Password: u.Password,
+  }
+}
