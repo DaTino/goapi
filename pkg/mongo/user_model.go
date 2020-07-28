4138a780054972ca2a3bccca7c03bc7e92888f71
DaTino <36283321+DaTino@users.noreply.github.com>
2020-07-28 13:08:44 -0500

    added session.go in correct spot, fixed user_model and user_service

--- pkg/mongo/user_model.go
+++ pkg/mongo/user_model.go
@@ -1,38 +1,37 @@
 package mongo
 
 import (
-  "goapi/pkg"
-  "gopkg.in/mgo.v2/bson"
-  "gopkg.in/mgo.v2"
+	"goapi/pkg"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type userModel struct {
-  Id        bson.ObjectId `bson:"_id,omitempty"`
-  Username  string
-  Password  string
+	Id       bson.ObjectId `bson:"_id,omitempty"`
+	Username string
+	Password string
 }
 
-func userModelIndex() mgo.Idex {
-  return mgo.Index{
-    Key:        []string{"username"},
-    Unique:     true,
-    DropDups:   true,
-    Background: true,
-    Sparse:     true,
-  }
+func userModelIndex() mgo.Index {
+	return mgo.Index{
+		Key:        []string{"username"},
+		Unique:     true,
+		DropDups:   true,
+		Background: true,
+		Sparse:     true,
+	}
 }
 
 func newUserModel(u *root.User) *userModel {
-  return &userModel{
-    Username: u.Username,
-    Password: u.Password
-  }
+	return &userModel{
+		Username: u.Username,
+		Password: u.Password}
 }
 
-func(u *userModel) toRootUser() *root.User {
-  return &root.User{
-    Id: u.Id.Hex(),
-    Username: u.Username,
-    Password: u.Password,
-  }
+func (u *userModel) toRootUser() *root.User {
+	return &root.User{
+		Id:       u.Id.Hex(),
+		Username: u.Username,
+		Password: u.Password}
 }
